d49d84b69d0e69d2a3c8dab8d0a3f521a959093b
James Watling <james.watling@gmail.com>
2022-01-24 17:13:56 +0000

    Hopefully fixed CRACO problems

--- craco.config.js
+++ craco.config.js
@@ -6,5 +6,23 @@ module.exports = {
                 require("autoprefixer")
             ]
         }
+    },
+    // webpack: {
+    //     rules: [
+    //         {
+    //             use: {
+    //                 loader: "babel-loader",
+    //                 options: {
+    //                     presets: ["@babel/preset-env"],
+    //                     ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"]
+    //                 }
+    //             }
+    //         }
+    //     ]
+    // }
+    babel: {
+        loaderOptions: {
+            ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"]
+        }
     }
 }
